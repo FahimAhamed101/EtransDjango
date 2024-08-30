60149ca10cf35a9f8acc306824eeedc758ccf043
fahim <fahimahamedweb@gmail.com>
2024-08-30 22:29:19 +0600

    cart count added

--- EtransDjango/settings.py
+++ EtransDjango/settings.py
@@ -121,7 +121,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-                      'category.context_list.menu_links',
+                      'category.context_list.menu_links', 'cart.context_processors.counter',
             ],
              "builtins": ['blog.templatetags.tag_cloud',
                          'blog.templatetags.markdown_processing', ] 
--- EtransDjango/views.py
+++ EtransDjango/views.py
@@ -5,7 +5,7 @@ from cart.models import CartItem
 
 from .settings import MEDIA_ROOT
 from category.models import banner,banneractive,Category_Offer,bannertwo
-
+from blog.models import Post
 
 def home(request):
     products = Product.objects.order_by('-created_date').filter(is_available=True,Is_featured=True)
@@ -36,18 +36,20 @@ def home(request):
             else:
                 pass
     
-
+    posts = Post.objects.filter(status='published') 
     bannerstwo = bannertwo.objects.filter(is_selected =True).order_by('id') 
     banners = banner.objects.filter(is_selected =True).order_by('id')
     banneractives = banneractive.objects.filter(is_selected =True).order_by('id')
-    user = request.user
+    
+    """ user = request.user
     if user.is_active == True:
         cart_count = CartItem.objects.filter(user=request.user,is_active=True).count()
     else:
         cart_count = 0
-
+'cart_count':cart_count,
     print(cart_count)
-    print(banners)
+    print(banners)"""
+    
     
 
 
@@ -59,7 +61,7 @@ def home(request):
         'products': products,
         'banners':banners,
         'banneractive':banneractives,
-        'cart_count':cart_count,
+        'posts':posts,
 
     }
     print(MEDIA_ROOT)
