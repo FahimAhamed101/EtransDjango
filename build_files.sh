80af3b2bef934cad1ba24380ffb1f0c6450f9f6d
fahim <fahimahamedweb@gmail.com>
2024-09-04 04:50:52 +0600

    deploying

--- build_files.sh
+++ build_files.sh
@@ -2,10 +2,12 @@
 
 
 
-
-pip install -r requirements.txt
-
-python3.10 manage.py migrate 
-python3.10 manage.py migrate sessions
-python3.10 manage.py collectstatic --no-input --clear
+python3 -m venv env
+source env/bin/activate
+python3 -m pip install -r requirements.txt
+python3 manage.py makemigrations
+python3 --version
+python3 manage.py migrate 
+python3 manage.py migrate sessions
+python3 manage.py collectstatic 
 DJANGO_SUPERUSER_PASSWORD=admin DJANGO_SUPERUSER_USERNAME=admin DJANGO_SUPERUSER_EMAIL=admin@gmail.com DJANGO_SUPERUSER_FIRST_NAME=fahim DJANGO_SUPERUSER_LAST_NAME=fahim python3.9 manage.py createsuperuser --noinput
