8efc7fa98e4805a4b2d3398fcd17ee9ca5049aa2
Fahim ahamed <54832397+FahimAhamed101@users.noreply.github.com>
2024-09-04 06:35:08 +0600

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -1,15 +1,11 @@
 # build_files.sh
 
 
-
-
-python3 -m pip install pipenv
-python3 -m pipenv shell
-python3 -m pip install --upgrade pip
-python3 -m pip install -r requirements.txt
+pip install --upgrade pip
+pip install -r requirements.txt
 python3 manage.py makemigrations
 python3 --version
 python3 manage.py migrate 
 python3 manage.py migrate sessions
 python3 manage.py collectstatic 
-DJANGO_SUPERUSER_PASSWORD=admin DJANGO_SUPERUSER_USERNAME=admin DJANGO_SUPERUSER_EMAIL=admin@gmail.com DJANGO_SUPERUSER_FIRST_NAME=fahim DJANGO_SUPERUSER_LAST_NAME=fahim python3.9 manage.py createsuperuser --noinput
+DJANGO_SUPERUSER_PASSWORD=admin DJANGO_SUPERUSER_USERNAME=admin DJANGO_SUPERUSER_EMAIL=admin@gmail.com DJANGO_SUPERUSER_FIRST_NAME=fahim DJANGO_SUPERUSER_LAST_NAME=fahim python3 manage.py createsuperuser --noinput
