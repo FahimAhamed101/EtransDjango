89cfd42df87340cb3906ab1f11e1c5749546726b
fahim <fahimahamedweb@gmail.com>
2024-09-04 23:48:40 +0600

    changes done

--- EtransDjango/settings.py
+++ EtransDjango/settings.py
@@ -72,6 +72,7 @@ MIDDLEWARE = [
     # Add the account middleware:
     "allauth.account.middleware.AccountMiddleware",
       "django_htmx.middleware.HtmxMiddleware",
+       "whitenoise.middleware.WhiteNoiseMiddleware",
 ]
 SOCIALACCOUNT_PROVIDERS = {
    
@@ -230,6 +231,8 @@ EMAIL_PORT = config('EMAIL_PORT', cast=int)
 EMAIL_HOST_USER = config('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
 EMAIL_USE_TLS = config('EMAIL_USE_TLS', cast=bool)
+
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 CLOUDINARY_STORAGE = {
     'CLOUD_NAME': 'fahim1213456',
