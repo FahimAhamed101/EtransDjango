db1435ef073d50fe7cfc228f447939aa5c8a8845
fahim <fahimahamedweb@gmail.com>
2024-09-04 23:58:22 +0600

    changes done

--- build_files.sh
+++ build_files.sh
@@ -1,15 +1,11 @@
 # build_files.sh
 
 
-<<<<<<< HEAD
-
-=======
->>>>>>> 8efc7fa98e4805a4b2d3398fcd17ee9ca5049aa2
 pip install --upgrade pip
 pip install -r requirements.txt
-python3 manage.py makemigrations
-python3 --version
-python3 manage.py migrate 
-python3 manage.py migrate sessions
-python3 manage.py collectstatic --clear
-DJANGO_SUPERUSER_PASSWORD=admin DJANGO_SUPERUSER_USERNAME=admin DJANGO_SUPERUSER_EMAIL=admin@gmail.com DJANGO_SUPERUSER_FIRST_NAME=fahim DJANGO_SUPERUSER_LAST_NAME=fahim python3 manage.py createsuperuser --noinput
+python manage.py makemigrations
+python --version
+python manage.py migrate 
+python manage.py migrate sessions
+python manage.py collectstatic
+DJANGO_SUPERUSER_PASSWORD=admin DJANGO_SUPERUSER_USERNAME=admin DJANGO_SUPERUSER_EMAIL=admin@gmail.com DJANGO_SUPERUSER_FIRST_NAME=fahim DJANGO_SUPERUSER_LAST_NAME=fahim python manage.py createsuperuser --noinput
