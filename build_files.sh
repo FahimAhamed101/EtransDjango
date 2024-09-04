5a4a228087baab49c3d2e5601e2d46d315fdb34d
fahim <fahimahamedweb@gmail.com>
2024-09-04 23:53:42 +0600

    changes done

--- build_files.sh
+++ build_files.sh
@@ -11,5 +11,5 @@ python3 manage.py makemigrations
 python3 --version
 python3 manage.py migrate 
 python3 manage.py migrate sessions
-python3 manage.py collectstatic 
+python3 manage.py collectstatic --clear
 DJANGO_SUPERUSER_PASSWORD=admin DJANGO_SUPERUSER_USERNAME=admin DJANGO_SUPERUSER_EMAIL=admin@gmail.com DJANGO_SUPERUSER_FIRST_NAME=fahim DJANGO_SUPERUSER_LAST_NAME=fahim python3 manage.py createsuperuser --noinput
