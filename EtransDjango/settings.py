86c916e37011964d635a633764ec0d4be7d25a0b
fahim <fahimahamedweb@gmail.com>
2024-09-05 00:18:53 +0600

    changes done

--- EtransDjango/settings.py
+++ EtransDjango/settings.py
@@ -216,9 +216,9 @@ STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
 #media files
-MEDIA_URL = '/media/'
+#MEDIA_URL = '/media/'
 
-MEDIA_ROOT = BASE_DIR/'media'
+#MEDIA_ROOT = BASE_DIR/'media'
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
