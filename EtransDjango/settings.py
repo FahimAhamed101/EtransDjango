2a4e857a7b75f9673d632c697e144a3c0849d267
fahim <fahimahamedweb@gmail.com>
2024-10-04 00:43:35 +0600

    publish store

--- EtransDjango/settings.py
+++ EtransDjango/settings.py
@@ -217,7 +217,7 @@ print(STATIC_ROOT)
 #media files
 MEDIA_URL = '/media/'
 
-#MEDIA_ROOT = BASE_DIR/'media'
+MEDIA_ROOT = BASE_DIR/'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
