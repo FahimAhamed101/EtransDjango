fa3abad1c633fde6d5a15339872b9de21241c7d2
fahim <fahimahamedweb@gmail.com>
2024-10-08 23:02:20 +0600

    deploy

--- EtransDjango/settings.py
+++ EtransDjango/settings.py
@@ -143,15 +143,15 @@ WSGI_APPLICATION = 'EtransDjango.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
+"""DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
-}
-"""DATABASES = {
-    'default': dj_database_url.parse(os.getenv('DATABASE_URL'), conn_max_age=600),
 }"""
+DATABASES = {
+    'default': dj_database_url.parse(os.getenv('DATABASE_URL'), conn_max_age=600),
+}
 
 SITE_ID =1
 ACCOUNT_EMAIL_REQUIRED=True
